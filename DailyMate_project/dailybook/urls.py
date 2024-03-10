67867a77d49c94a375e0c763bf4101b923fae00d
Teodors Chaly <iteo.chaly@gmail.com>
2024-03-10 19:25:15 +0200

    HTML connection

---
+++ DailyMate_project/dailybook/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import EntryListView, EntryDetailView, EntryCreateView
+
+urlpatterns = [
+    path('', EntryListView.as_view(), name='entry_list'),
+    path('entry/<int:pk>/', EntryDetailView.as_view(), name='entry_detail'),
+    path('entry/add/', EntryCreateView.as_view(), name='entry_create'),
+]
--- DailyMate_project/dailybook/views.py
+++ DailyMate_project/dailybook/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView
+from .models import Entry
+from django.urls import reverse_lazy
 
-# Create your views here.
+class EntryListView(ListView):
+    model = Entry
+    template_name = 'entry_list.html'
+
+class EntryDetailView(DetailView):
+    model = Entry
+    template_name = 'entry_detail.html'
+
+class EntryCreateView(CreateView):
+    model = Entry
+    template_name = 'entry_form.html'
+    fields = ['title', 'content']
+    success_url = reverse_lazy('entry_list')
