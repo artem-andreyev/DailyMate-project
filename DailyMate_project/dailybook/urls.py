7c6199889c65fb2896f2711a2412130bcca5bf70
Teodors Chaly <iteo.chaly@gmail.com>
2024-03-21 12:05:38 +0200

    Privacy for dailybook

--- DailyMate_project/dailybook/urls.py
+++ DailyMate_project/dailybook/urls.py
@@ -1,17 +1,19 @@
 from django.urls import path
+from django.views.generic import RedirectView
+
 from .views import *
 from django.contrib.auth import views as auth_views
+
 urlpatterns = [
-    path('', note_list, name='note_list'),
-    path('daily-book/', dailybook_list, name='dailybook_list'),
-
-    path('daily-book/add/', DailybookCreateView.as_view(), name='dailybook_create'),
-    path('daily-book/<int:pk>/', DailybookDetailView.as_view(), name='dailybook_detail'),
-    path('daily-book/<int:pk>/edit/', DailybookUpdateView.as_view(), name='dailybook_edit'),
-
-    path('note/<int:pk>/', EntryDetailView.as_view(), name='entry_detail'),
-    path('note/add/', EntryCreateView.as_view(), name='entry_create'),
-    path('note/<int:pk>/edit/', EntryUpdateView.as_view(), name='entry_edit'),
+    path('', RedirectView.as_view(url='/profile/')),
+    path('<str:username>/notes/', note_list, name='note_list'),
+    path('<str:username>/daily-book/', dailybook_list, name='dailybook_list'),
+    path('<str:username>/daily-book/add/', DailybookCreateView.as_view(), name='dailybook_create'),
+    path('<str:username>/daily-book/<int:pk>/', DailybookDetailView.as_view(), name='dailybook_detail'),
+    path('<str:username>/daily-book/<int:pk>/edit/', DailybookUpdateView.as_view(), name='dailybook_edit'),
+    path('<str:username>/note/<int:pk>/', EntryDetailView.as_view(), name='entry_detail'),
+    path('<str:username>/note/add/', EntryCreateView.as_view(), name='entry_create'),
+    path('<str:username>/note/<int:pk>/edit/', EntryUpdateView.as_view(), name='entry_edit'),
 
     path('profile/', profile_view, name='profile'),
     path('register/', RegisterView.as_view(), name='register'),
--- DailyMate_project/dailybook/views.py
+++ DailyMate_project/dailybook/views.py
@@ -1,77 +1,121 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, FormView
-from .models import Entry, Dailybook, UserProfile
-from .forms import EntryForm, DailybookForm, RegisterForm
+from django.views.generic import DetailView, CreateView, UpdateView, FormView
+from .models import Entry, Dailybook
+from .forms import DailybookForm, RegisterForm
 from django.urls import reverse_lazy
 from django.contrib.auth.decorators import login_required
 
-
-# class EntryListView(ListView):
-#     model = Entry
-#     template_name = 'note_list.html'
-
-
-# class DailybookListView(ListView):
-#     model = Dailybook
-#     template_name = 'dailybook_list.html'
+from django.contrib.auth.models import User
+from django.http import Http404, HttpResponseForbidden
 
 
 @login_required
-def dailybook_list(request):
-    dailybooks = Dailybook.objects.filter(author=request.user)
+def dailybook_list(request, username):
+    try:
+        user = User.objects.get(username=username)
+    except User.DoesNotExist:
+        raise Http404("User does not exist")
+
+    dailybooks = Dailybook.objects.filter(author=user)
     return render(request, 'dailybook_list.html', {'dailybooks': dailybooks})
 
-@login_required
-def note_list(request):
-    entries = Entry.objects.filter(author=request.user)
-    return render(request, 'note_list.html', {'entries': entries})
 
-class EntryDetailView(DetailView):
+@login_required
+def note_list(request, username):
+    if request.user.username != username:
+        return HttpResponseForbidden()
+    else:
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            raise Http404("User does not exist")
+
+        entries = Entry.objects.filter(author=user)
+        return render(request, 'note_list.html', {'entries': entries})
+
+
+class EntryDetailView(LoginRequiredMixin, DetailView):
     model = Entry
     template_name = 'note_detail.html'
+    context_object_name = 'entry'
+
+    def dispatch(self, request, *args, **kwargs):
+        username = kwargs.get('username')
+        if request.user.username != username:
+            return HttpResponseForbidden()
+        return super().dispatch(request, *args, **kwargs)
 
 
-class DailybookDetailView(DetailView):
+class DailybookDetailView(LoginRequiredMixin, DetailView):
     model = Dailybook
     template_name = 'dailybook_detail.html'
 
+    def dispatch(self, request, *args, **kwargs):
+        username = kwargs.get('username')
+        if request.user.username != username:
+            return HttpResponseForbidden()
+        return super().dispatch(request, *args, **kwargs)
 
-class EntryCreateView(CreateView):
+
+class EntryCreateView(LoginRequiredMixin, CreateView):
     model = Entry
-    form_class = EntryForm
+    fields = ['title', 'content']
     template_name = 'note_form.html'
-    success_url = reverse_lazy('note_list')
-
-    def form_valid(self, form):
-        # Устанавливаем текущего пользователя в качестве автора записи
-        form.instance.author = self.request.user
-        return super().form_valid(form)
-
-
-
-class DailybookCreateView(CreateView):
-    model = Dailybook
-    form_class = DailybookForm
-    template_name = 'dailybook_form.html'
-    success_url = reverse_lazy('dailybook_list')
 
     def form_valid(self, form):
         form.instance.author = self.request.user
+        form.instance.username = self.kwargs['username']
         return super().form_valid(form)
 
+    def get_success_url(self):
+        return reverse_lazy('note_list', kwargs={'username': self.kwargs['username']})
 
-class DailybookUpdateView(UpdateView):
+
+class DailybookCreateView(LoginRequiredMixin, CreateView):
     model = Dailybook
     form_class = DailybookForm
     template_name = 'dailybook_form.html'
-    success_url = reverse_lazy('dailybook_list')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        form.instance.username = self.kwargs['username']
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('dailybook_list', kwargs={'username': self.kwargs['username']})
 
 
-class EntryUpdateView(UpdateView):
+class DailybookUpdateView(LoginRequiredMixin, UpdateView):
+    model = Dailybook
+    form_class = DailybookForm
+    template_name = 'dailybook_form.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        username = kwargs.get('username')
+        entry = self.get_object()
+        if request.user != entry.author or request.user.username != username:
+            return HttpResponseForbidden()
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_success_url(self):
+        return reverse_lazy('dailybook_list', kwargs={'username': self.kwargs['username']})
+
+
+class EntryUpdateView(LoginRequiredMixin, UpdateView):
     model = Entry
-    form_class = EntryForm
+    fields = ['title', 'content']
     template_name = 'note_form.html'
-    success_url = reverse_lazy('note_list')
+
+    def dispatch(self, request, *args, **kwargs):
+        username = kwargs.get('username')
+        entry = self.get_object()
+        if request.user != entry.author or request.user.username != username:
+            return HttpResponseForbidden()
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_success_url(self):
+        return reverse_lazy('note_list', kwargs={'username': self.kwargs['username']})
 
 
 @login_required
